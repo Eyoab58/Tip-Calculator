86944842b6f1aa4f25a779d5f38bd9c906deada0
Emma Paris <eyoabasrat@gmail.com>
2020-08-20 12:55:24 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Eyoab Asrat on 8/20/20.
+//  Copyright © 2020 Eyoab Asrat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
